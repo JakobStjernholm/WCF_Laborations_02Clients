9f1e59a9a5d3b61a7f3f345925f33dd179b87a4f
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-19 15:57:31 +0100

    Done

---
+++ ConsoleClientDays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClientDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateDaysClassClient client = new CalculateDaysClassClient();
+            Console.WriteLine("Enter birthday (YYYY-MM-DD):");
+            var date = Console.ReadLine();
+            Console.WriteLine(client.GetNextDateWhen1000(date));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleClientWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClientWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WordClassClient client = new WordClassClient();
+            Console.WriteLine("Skriv in en text, så räknar vi ut antalet ord!");
+            var result = client.GetNumberOfWords(Console.ReadLine());
+            Console.WriteLine("Antalet ord: " + result);
+            Console.ReadKey();
+
+
+        }
+    }
+}
